45a6cdbd977baefd056e107897aa7de8d39bede8
커비 <pkirby0426@gmail.com>
2024-11-10 22:16:49 +0900

    📝 Add or update documentation.

---
+++ career/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>경력기술서</title>
+</head>
+<body>
+    <div id="career">
+        <div>20</div>
+    </div>
+</body>
+</html>
